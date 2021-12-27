8d3a328097ee2d5e8c111b3e5fd4ab32f37a4b7e
Roni <rtlockwoodwork@gmail.com>
2021-12-27 16:43:24 -0700

    background

--- Components/Header.js
+++ Components/Header.js
@@ -2,38 +2,47 @@ import Link from "next/link";
 import { useContext } from "react";
 
 import AuthContext from "../context/AuthContext";
+import Emoji from "./Emoji";
 // import UploadModal from "./uploadModal";
 
 export default function Header() {
 
     const { user } = useContext(AuthContext)
     return (
-        <div className="w-full  py-4 text-center bg-blue-900">
-            <div>
-                <Link href="/">
-                    <a className="text-purple-50 text-bold text-4xl">Brave_Cards</a>
-                </Link>
-                <h2 className="text-2xl text-green-50">Helping you find inspiration!</h2>
-            </div>
+        <div className="w-full  py-4 text-center bg-regal-gold/75 text-white">
+            <Emoji symbol="\u{1F451}" label="star" />
             
-<div className="flex justify-evenly items-center">
-    
-    <div className="text-purple-50"> {user ? (
-        <div >
-    
-        <Link href="/account" >
-            <a className=""><span className="text-black font-bold">User:</span> {user.email}</a>
-        </Link>
+        <div className="flex justify-around items-center text-center">
+            <Emoji symbol="\u{1F451}" label="star" />
+            <div className=""> {user ? (
+                <div >
+            
+                <Link href="/account" >
+                    <a className=""><span className="text-black font-bold">User:</span> {user.email}</a>
+                </Link>
+                </div>
+            ) : (
+                <Link href="/login">
+                    <a>Login</a></Link>
+            )}
+                </div>
+                <div>
+                <Link href="/">
+                    <a className="text-bold text-4xl">Brave_Cards</a>
+                </Link>
+                <h2 className="text-2xl text-green-500">Helping you find inspiration!</h2>
+            </div>
+            {/* z<UploadModal /> */}
+            <div className="text-center">
+                <Link  href="/resources" ><a className="text-xl  hover:text-green-600 hover:text-2xl pr-4">Resource Links</a></Link>
+                <Link  href="/contact" ><a className="text-xl hover:text-green-600 hover:text-2xl">Contact</a></Link>
+            </div>
+            <Emoji symbol="\u{1F451}" label="star" />
         </div>
-    ) : (
-        <Link href="/login">
-            <a>Login</a></Link>
-    )}
-        </div>
-    {/* z<UploadModal /> */}
-    <Link  href="/resources" ><a className="text-xl text-green-50 hover:bg-blue-900 hover:text-2xl">Resource Links</a></Link>
-    <Link  href="/contact" ><a className="text-xl text-green-50 hover:bg-blue-900 hover:text-2xl">Contact</a></Link>
-</div>
         </div>
     )
 }
+
+// divider crown <a href='https://www.freepik.com/vectors/border'>Border vector created by starline - www.freepik.com</a>
+// hearts <a href='https://www.freepik.com/vectors/watercolor'>Watercolor vector created by tartila - www.freepik.com</a> 
+// background pattern <svg xmlns='http://www.w3.org/2000/svg'  width='90' height='90' viewBox='0 0 90 90'><rect fill='#330033' width='90' height='90'/><circle fill-opacity='1' fill='#06B'  cx='45' cy='45' r='10'/><g fill='#800'  fill-opacity='1'><circle  cx='0' cy='90' r='10'/><circle  cx='90' cy='90' r='10'/><circle  cx='90' cy='0' r='10'/><circle  cx='0' cy='0' r='10'/></g></svg>
--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,21 @@ export default function Home({cards}) {
   return (
     <div className="main h-full pt-4 pb-36">
   
-        <div className="header my-4 text-center text-blue-900 font-extrabold">
+        <div className="header w-3/4 m-auto py-4 text-center text-white bg-regal-blue/50 font-extrabold">
           <h2 >These Women are </h2>
           <h2 className="text-4xl uppercase">inspiring, courageous, rebellious, kind, compassionate, fierce, and much more.</h2>
           <h4>Click on a card to learn more about each person!</h4>
         </div>
         
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 h-full mb-20 ml-10">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 h-full my-8 ml-10">
        {cards &&
         cards.map((card) => (
           <Link href={`/${card.Slug}`} key={card.id} passHref>
                <div  className="card w-64 shadow-2xl cursor-pointer rounded-lg">
                 <div className="front w-64 rounded-lg m-auto">
                 <div className="w-full ">
-                <Image className="m-auto  rounded-lg overflow-hidden" layout="fixed" sizes="50vh" src={fromImageToUrl(card.mainImage)}  alt={card.name} width={280} height={275}/>
+                <img src={fromImageToUrl(card.mainImage)}  alt={card.name} className="w-full h-64"  />
+                {/* <Image className="m-auto  rounded-lg overflow-hidden" layout="fixed" sizes="50vh" src={fromImageToUrl(card.mainImage)}  alt={card.name} width={280} height={275}/> */}
                 </div>
                 <div className="relative w-16 h-16 bg-green-900 rounded-full flex justify-center items-center text-center text-white p-5 shadow-xl -mt-16 ml-2">
                   
@@ -36,7 +37,7 @@ export default function Home({cards}) {
               </div>
               </div>
 
-              <div  className="back text-purple-50 max-w-md w-64 h-72 shadow-lg  bg-white">
+              <div  className="back rounded-lg text-purple-50 max-w-md w-64 h-72 shadow-lg  bg-white">
               <div className="px-6 bg-blue-900 ">
                 <div className="font-bold text-2xl  text-center">{card.name}</div>
                 <div className="">
@@ -46,7 +47,7 @@ export default function Home({cards}) {
                 
               </div>
                   <div className="px-4 h-full text-black overflow-hidden">
-                   
+                   <div className="mb-2">Quote - {card.quote1}</div>
                     <span className="font-bold">My Why:</span> click to learn more
                       {/* {card.theWhy}  */}
                     </div> 
